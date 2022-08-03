97973f2683f8af5af7f1010a91a6c9c24f8b4ea0
rako2002 <45415103+rako2002@users.noreply.github.com>
2022-08-03 12:12:14 +1000

    Create 01 schemas.sql

---
+++ Data Vault automation (Raw Vault)/01 schemas.sql
@@ -0,0 +1,30 @@
+/****** Object:  Schema [BV]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [BV]
+GO
+/****** Object:  Schema [CL]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [CL]
+GO
+/****** Object:  Schema [EOT]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [EOT]
+GO
+/****** Object:  Schema [LKP]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [LKP]
+GO
+/****** Object:  Schema [mtdta]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [mtdta]
+GO
+/****** Object:  Schema [RV_CMS]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [RV_CMS]
+GO
+/****** Object:  Schema [RV_EOT]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [RV_EOT]
+GO
+/****** Object:  Schema [STG_BV]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [STG_BV]
+GO
+/****** Object:  Schema [STG_CMS]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [STG_CMS]
+GO
+/****** Object:  Schema [STG_EOT]    Script Date: 12/03/2020 12:24:07 PM ******/
+CREATE SCHEMA [STG_EOT]
+GO
